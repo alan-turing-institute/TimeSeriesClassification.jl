8717e82a7ba9922384fd8124d28eae9f8840d061
aa25desh <aadeshdeshmukh25@gmail.com>
2020-09-18 20:59:27 +0530

    update

--- src/interval_based_forest.jl
+++ src/interval_based_forest.jl
@@ -55,7 +55,7 @@ function InvFeatureGen(Xmatrix::Array, n_trees::Int, min_interval::Int, random_s
     n_instance, series_length = size(Xmatrix)
     Interval_features = Array{Array{Float64,2},1}()
     n_intervals = floor(Int, sqrt(series_length))
-    Intervals = zeros(Int, n_trees, 3*n_intervals, 2)
+    Intervals = zeros(Int, n_trees, n_intervals, 2)
     for i in range(1, stop = n_trees)
        interval_feature = Array{Float64,2}(undef, 3*n_intervals, n_instance)
        for j in range(1, stop = n_intervals)
